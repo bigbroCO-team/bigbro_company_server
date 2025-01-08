55df5e3259e041abc78d84eafe8b859cc5f52449
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-08 19:01:19 +0900

    Edit env

--- src/core/settings/prod.py
+++ src/core/settings/prod.py
@@ -23,6 +23,6 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': f'redis://{os.environ.get('REDIS_HOST')}:{os.environ.get('PORT')}',
+        'LOCATION': f'redis://{os.environ.get('REDIS_HOST')}:{os.environ.get('REDIS_PORT')}',
     }
 }
