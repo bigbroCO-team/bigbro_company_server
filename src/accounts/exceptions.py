4ff0ab4ecc31445b0473a03e1f67de784f72ffe2
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-15 20:55:59 +0900

    Edit migration, exception

--- src/accounts/exceptions.py
+++ src/accounts/exceptions.py
@@ -3,8 +3,3 @@ from core.exceptions import BaseCustomException
 
 class LoginException:
     LoginFailException = BaseCustomException(code=400, detail='Fail to login')
-    UserNameAlreadyExistsException = BaseCustomException(code=400, detail='Username already exists')
-    PasswordTooShortException = BaseCustomException(code=400, detail='Password must be at least 6 characters long')
-
-    @staticmethod
-    def kakaoOAuthLoginException(detail): return BaseCustomException(code=400, detail=detail)
