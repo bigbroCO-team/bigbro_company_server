2535e34aaff63aef5c313744e57dbf839de26bc9
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-18 17:44:10 +0900

    Edit address excpetion

--- src/address/exceptions.py
+++ src/address/exceptions.py
@@ -1,5 +1,5 @@
 from core.exceptions import BaseCustomException
 
 class AddressException:
-    addressNotFound = BaseCustomException(code=400, detail="Address Not Found.")
+    addressNotFound = BaseCustomException(code=404, detail="Address Not Found.")
     phoneNumberIsNotValid = BaseCustomException(code=400, detail="Phone Number Is Not Valid.")
