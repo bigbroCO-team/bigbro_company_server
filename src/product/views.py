2beb9cc19aab74fba77df71c6aa10caa8d242cff
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-20 19:21:02 +0900

    Delete unnecessary import

--- src/product/views.py
+++ src/product/views.py
@@ -1,6 +1,5 @@
 from django.db import transaction
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
