d977628c34dd2168b8fc88e33337798e2d3710e8
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-23 15:41:04 +0900

    Cart model pk cascade to set null

--- src/cart/models.py
+++ src/cart/models.py
@@ -6,8 +6,8 @@ from product.models import Product, ProductOption
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    option = models.ForeignKey(ProductOption, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    option = models.ForeignKey(ProductOption, on_delete=models.SET_NULL, null=True)
     count = models.PositiveSmallIntegerField()
 
     class Meta:
