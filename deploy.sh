7fe298523bbd875d7d7fa87d0491288e71bbb181
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-27 04:44:23 +0900

    Add healthckeck, rollback

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,39 @@
 
 IS_BLUE=$(docker ps | grep blue)
 
+# Blue-green 배포
 if [ -z "$IS_BLUE" ]; then
     echo "Deploying blue"
 
     docker-compose -f infra/compose/docker-compose.yml up --build -d web-blue
     docker-compose -f infra/compose/docker-compose.yml down web-green
-    docker system prune -af --volumes=false
 else
     echo "Deploying green"
     docker-compose -f infra/compose/docker-compose.yml up --build -d web-green
     docker-compose -f infra/compose/docker-compose.yml down web-blue
-    docker system prune -af --volumes=false
 fi
 
+# Health check
+if curl http://localhost:80/health-check | grep 502; then
+    echo "Health check failed"
+    
+    # Rollback
+    if [ -z "$IS_BLUE" ]; then
+        echo "Rolling back to blue"
+
+        docker-compose -f infra/compose/docker-compose.yml up -d web-green
+        docker-compose -f infra/compose/docker-compose.yml down web-blue
+        docker system prune -af --volumes=false
+        exit 1
+    else
+        echo "Rolling back to green"
+
+        docker-compose -f infra/compose/docker-compose.yml up -d web-blue
+        docker-compose -f infra/compose/docker-compose.yml down web-green
+        docker system prune -af --volumes=false
+        exit 1
+    fi
+else
+    echo "Health check passed"
+    docker system prune -af --volumes=false
+fi
