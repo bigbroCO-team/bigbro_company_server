e9a76608008c9f2ac5d4fefa54c16eaf7ed3305d
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-27 04:30:56 +0900

    Add docker prune

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,11 @@ if [ -z "$IS_BLUE" ]; then
 
     docker-compose -f infra/compose/docker-compose.yml up --build -d web-blue
     docker-compose -f infra/compose/docker-compose.yml down web-green
+    docker system prune -af --volumes=false
 else
     echo "Deploying green"
     docker-compose -f infra/compose/docker-compose.yml up --build -d web-green
     docker-compose -f infra/compose/docker-compose.yml down web-blue
+    docker system prune -af --volumes=false
 fi
+
