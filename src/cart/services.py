bea18e74d425200c16b50d978c5fe724734b5830
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-21 14:01:53 +0900

    update: settings base.py

---
+++ src/cart/services.py
@@ -0,0 +1,38 @@
+from django.db import transaction
+
+from cart.models import Cart
+from product.models import ProductOption
+
+
+class CartService:
+    def __init__(self):
+        pass
+
+    @transaction.atomic
+    def create(self, user, product, option, count):
+
+        option_obj = ProductOption.objects.get(
+            product_id=product,
+            name=option
+        )
+
+        exists_cart = Cart.object.filter(
+            user=user,
+            product=product,
+            option=option_obj
+        ).first()
+
+        if exists_cart:
+            exists_cart.count += count
+            exists_cart.save()
+            return exists_cart
+        else:
+            new_cart = Cart(
+                user=user,
+                product=product,
+                option=option_obj,
+                count=count
+            )
+            new_cart.full_clean()
+            new_cart.save()
+            return new_cart
