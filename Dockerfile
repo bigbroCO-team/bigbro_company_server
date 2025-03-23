cc14af9d03b63cfbf9246ab9c02e284a01961418
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-23 23:00:20 +0900

    fix: start.sh

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN pip install poetry
 
 RUN poetry install --no-root
 
+RUN poetry run python src/manage.py migrate
+
 EXPOSE 80
 
 CMD [ "poetry", "run", "gunicorn", "--chdir", "./src/",  "-b", "0.0.0.0:80", "core.wsgi:application" ]
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+mkdir -p ~/bigbro_company_server
+
 cd ~/bigbro_company_server
+
+if [ ! -d ".git" ]; then
+  git clone -b develop https://github.com/bigbroCO-team/bigbro_company_server .
+else
+  git pull origin develop
+fi
 
 docker build -t bigbro .
 
