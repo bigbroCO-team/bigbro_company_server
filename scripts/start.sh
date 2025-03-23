d4db5b8011c76a5380346314e1b9d5bee4fce509
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-23 23:15:20 +0900

    fix: cd

--- scripts/start.sh
+++ scripts/start.sh
@@ -17,3 +17,5 @@ docker stop bigbro || true
 docker rm bigbro || true
 
 docker run -d -p 80:80 --name bigbro --env-file ~/.env bigbro
+
+docker exec bigbro poetry run python /app/src/manage.py migrate
