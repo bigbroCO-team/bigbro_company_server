96b273b0ea457cfd61dcad4f5ce4992faf081c0d
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-31 10:19:26 +0900

    fix: url

--- src/core/settings/stage.py
+++ src/core/settings/stage.py
@@ -35,4 +35,4 @@ CACHES = {
 }
 
 KAKAO_CLIENT_REDIRECT_URL = 'https://stage.bigbro.company/main'
-KAKAO_REDIRECT_URI = 'https://stage.api.bigbro.company/account/auth/kakao/callback'
+KAKAO_REDIRECT_URI = 'http://stage.api.bigbro.company/account/auth/kakao/callback'
