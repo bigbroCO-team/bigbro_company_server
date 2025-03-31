a806d9007e268ce200b701634f1116144f71220a
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-31 14:04:13 +0900

    fix

--- src/core/settings/stage.py
+++ src/core/settings/stage.py
@@ -18,12 +18,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': os.environ.get("POSTGRES_PORT"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'bigbro'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'bigbro-postgres'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
