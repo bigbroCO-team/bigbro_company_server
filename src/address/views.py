a89603f282287d68eb3a4f0529157d0874a9653b
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-03 02:33:22 +0900

    refactor: address views

--- src/address/views.py
+++ src/address/views.py
@@ -1,9 +1,9 @@
 from django.db import transaction
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 
 from core.authentication import CsrfExemptSessionAuthentication
 from .exceptions import AddressNotFoundException
@@ -18,14 +18,14 @@ class AddressView(APIView):
     def get(self, request: Request) -> Response:
         address = Address.objects.filter(user_id=request.user)
         serializer = AddressSerializer(address, many=True)
-        return Response(serializer.data, status=HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     @transaction.atomic
     def post(self, request: Request) -> Response:
         serializer = AddressSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
-        return Response(status=HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)
     
     @transaction.atomic
     def put(self, request: Request, address_id: int) -> Response:
@@ -35,7 +35,7 @@ class AddressView(APIView):
         serializer =  AddressSerializer(address, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(status=HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
     
     @transaction.atomic
     def delete(self, request: Request, address_id: int) -> Response:
@@ -43,4 +43,4 @@ class AddressView(APIView):
         if not address:
             raise AddressNotFoundException()
         address.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
