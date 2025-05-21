f86f85284f9ebd40b87c78fd323481ecc1a84d26
Jueuunn7 <wndmsrla1234@gmail.com>
2025-05-21 20:21:12 +0900

    feat: myinfo view

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from .views.kakao_callback import KakaoLoginCallBackView
 from .views.kakao_login import KakaoLoginView
 from .views.logout import LogoutView
+from .views.my import MyInfoView
+
 
 urlpatterns = [
     path('/auth/kakao', KakaoLoginView.as_view(), name='auth-kakao'),
     path('/auth/kakao/callback', KakaoLoginCallBackView.as_view(), name='auth-kakao-callback'),
     path('/auth/logout', LogoutView.as_view()),
+    path('/my', MyInfoView.as_view()),
 ]
