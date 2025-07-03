0cd009d3b27d8485936bf4eace7f569d85deb1ae
Jueuunn7 <wndmsrla1234@gmail.com>
2025-07-03 16:16:00 +0900

    update: root urlpatterns

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,12 +4,14 @@ from django.urls import path, include
 from address.urls import address_router
 from accounts.urls import account_router
 from cart.urls import cart_router
+from order.urls import order_router
+from product.urls import product_router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(address_router.urls)),
     path("", include(account_router.urls)),
     path("", include(cart_router.urls)),
-    path("", include("product.urls")),
-    path("", include("order.urls")),
+    path("", include(order_router.urls)),
+    path("", include(product_router.urls)),
 ]
