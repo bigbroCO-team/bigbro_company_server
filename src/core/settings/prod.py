58c8eddf61166b7e9f9ac734cdd8003f3c24ce1b
Jueuunn7 <wndmsrla1234@gmail.com>
2025-07-03 16:21:47 +0900

    update: double quote

--- src/core/settings/prod.py
+++ src/core/settings/prod.py
@@ -27,7 +27,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": f"redis://{os.environ.get('REDIS_HOST')}:{os.environ.get('REDIS_PORT')}",
+        "LOCATION": f"redis://{os.environ.get("REDIS_HOST")}:{os.environ.get("REDIS_PORT")}",
     }
 }
 
