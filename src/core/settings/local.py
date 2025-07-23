56d919fd51d20672f6269bbe37121dad404d1445
Jueuunn7 <wndmsrla1234@gmail.com>
2025-07-23 23:24:47 +0900

    settings: cache

--- src/core/settings/local.py
+++ src/core/settings/local.py
@@ -14,12 +14,5 @@ DATABASES = {
     }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
-    }
-}
-
 KAKAO_CLIENT_REDIRECT_URL = "http://127.0.0.1:8000/"
 KAKAO_REDIRECT_URI = "http://127.0.0.1:8000/account/auth/kakao/callback"
